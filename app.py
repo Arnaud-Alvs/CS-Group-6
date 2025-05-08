4a118d79b880625a5bab8d106aeccaaaab58edfe
Arnaud-Alvs <arnaud.alves342@gmail.com>
2025-05-08 17:05:52 +0000

    hj

--- app.py
+++ app.py
@@ -365,4 +365,4 @@ if __name__ == "__main__":
     st.write("Please wait while we load the application...")
     
     # Redirect to the Home page
-    st.switch_page("pages/1_Home.py")
+    st.switch_page("./pages/1_Home.py")
